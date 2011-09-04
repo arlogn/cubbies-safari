c029ac00a48491a70980a8b3b6bda1513f4dcbb8
gattonero <claudio@bastardi.net>
2011-09-05 00:52:58 +0200

    update 1.0.1

--- cubbies.safariextension/cubbies.js
+++ cubbies.safariextension/cubbies.js
@@ -1,9 +1,30 @@
-
 /*****************************************************************************
  * THIS SNIPPET: Copyright 2004 ThoughtWorks, Inc
  * Licensed under the Apache License, Version 2.0 */
  function absolutify(url,baseUrl){if(/^\w+:/.test(url)){return url}var loc;try{loc=parseUrl(baseUrl)}catch(e){if(/^\w:\\/.test(baseUrl)){baseUrl="file:///"+baseUrl.replace(/\\/g,"/");loc=parseUrl(baseUrl)}else{throw new SeleniumError("baseUrl wasn't absolute: "+baseUrl);}}loc.search=null;loc.hash=null;if(/^\//.test(url)){loc.pathname=url;var result=reassembleLocation(loc);return result}if(!loc.pathname){loc.pathname="/"+url;var result=reassembleLocation(loc);return result}if(/\/$/.test(loc.pathname)){loc.pathname+=url;var result=reassembleLocation(loc);return result}loc.pathname=loc.pathname.replace(/[^\/\\]+$/,url);var result=reassembleLocation(loc);return result}var URL_REGEX=/^((\w+):\/\/)(([^:]+):?([^@]+)?@)?([^\/\?:]*):?(\d+)?(\/?[^\?#]+)?\??([^#]+)?#?(.+)?/;function parseUrl(url){var fields=['url',null,'protocol',null,'username','password','host','port','pathname','search','hash'];var result=URL_REGEX.exec(url);if(!result){throw new SeleniumError("Invalid URL: "+url);}var loc=new Object();for(var i=0;i<fields.length;i++){var field=fields[i];if(field==null){continue}loc[field]=result[i]}return loc}function reassembleLocation(loc){if(!loc.protocol){throw new Error("Not a valid location object: "+o2s(loc));}var protocol=loc.protocol;protocol=protocol.replace(/:$/,"");var url=protocol+"://";if(loc.username){url+=loc.username;if(loc.password){url+=":"+loc.password}url+="@"}if(loc.host){url+=loc.host}if(loc.port){url+=":"+loc.port}if(loc.pathname){url+=loc.pathname}if(loc.search){url+="?"+loc.search}if(loc.hash){var hash=loc.hash;hash=loc.hash.replace(/^#/,"");url+="#"+hash}return url}
 /*****************************************************************************/
+
+/*
+Copyright (C) 2011 by Diaspora, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 
 var dom = {};
 dom.query = jQuery.noConflict(true);
@@ -102,8 +123,8 @@ var Cubbies = {
     var stylesheet = document.createElement('style');
     stylesheet.innerHTML = "" +
       "img, #cubbies-overlay{ -webkit-transition-property: margin, box-shadow, z-index; -webkit-transition-duration: 0.1s; }\n" +
-      ".cubbies-selected{ z-index: 9999; -webkit-box-shadow: 3px 3px 8px -1px blue !important; cursor: pointer !important; margin: -3px 3px 3px -3px; }\n" +
-      ".cubbies-selected:active{ -webkit-box-shadow: 2px 2px 5px -1px darkblue !important; margin: -1px 1px 1px -1px; }\n" +
+      ".cubbies-selected{ z-index: 9999; -webkit-box-shadow: 3px 3px 8px -1px #e0e !important; cursor: pointer !important; margin: -3px 3px 3px -3px; }\n" +
+      ".cubbies-selected:active{ -webkit-box-shadow: 2px 2px 5px -1px #808 !important; margin: -1px 1px 1px -1px; }\n" +
       "#cubbies-overlay{ position: fixed; z-index: 9999; bottom: 30px; left: 30px; -webkit-box-shadow: 0 2px 3px rgba(0,0,0,0.8); border: none; }\n" +
       "#cubbies-overlay:hover{ -webkit-box-shadow: 0 2px 3px rgb(0,0,0); }"
     document.body.appendChild(stylesheet);
